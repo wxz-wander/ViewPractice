c08381ba2e2ea4d2c0c980b0b07f92e87ce6b04b
王超 <wangc@huitouche.com>
2019-03-16 15:09:30 +0800

    添加NestedScrollingParent和NestedScrollingChild的方法注释

--- app/src/main/java/com/example/viewpractice/widgets/NestedChildView.java
+++ app/src/main/java/com/example/viewpractice/widgets/NestedChildView.java
@@ -17,7 +17,15 @@ import androidx.core.view.ViewCompat;
 
 /**
  * 作者：wxz11 on 2019/3/8 17:58
- * Email : wangc@huitouche.com
+ *  NestedScrollingChild。
+ *  如果你有一个可以滑动的 View，需要被用来作为嵌入滑动的子 View，就必须实现本接口。
+ *  在此 View 中，包含一个 NestedScrollingChildHelper 辅助类。
+ *  NestedScrollingChild 接口的实现，基本上就是调用本 Helper 类的对应的函数即可，因为 Helper 类中已经实现好了 Child 和 Parent 交互的逻辑。原来的 View 的处理 Touch 事件，并实现滑动的逻辑大体上不需要改变。
+ *
+ * 需要做的就是，如果要准备开始滑动了，需要告诉 Parent，你要准备进入滑动状态了，调用 startNestedScroll()。
+ * 你在滑动之前，先问一下你的 Parent 是否需要滑动，也就是调用 dispatchNestedPreScroll()。如
+ * 果父类滑动了一定距离，你需要重新计算一下父类滑动后剩下给你的滑动距离余量。然后，你自己进行余下的滑动。
+ * 最后，如果滑动距离还有剩余，你就再问一下，Parent 是否需要在继续滑动你剩下的距离，也就是调用 dispatchNestedScroll()。
  */
 public class NestedChildView extends View implements NestedScrollingChild {
     private static final String TAG = "NestedChildView";
@@ -88,12 +96,22 @@ public class NestedChildView extends View implements NestedScrollingChild {
         return b;
     }
 
+    /**
+     * 设置嵌套滑动是否可用
+     * @param enabled :true表示view使用嵌套滚动,false表示禁用.
+     * */
     @Override
     public void setNestedScrollingEnabled(boolean enabled) {
 //        super.setNestedScrollingEnabled(enabled);
         childHelper.setNestedScrollingEnabled(true);
     }
 
+    /**
+     * 开始嵌套滑动
+     * @param axes :表示滚动的方向如:ViewCompat.SCROLL_AXIS_VERTICAL(垂直方向滚动)和
+     *     ViewCompat.SCROLL_AXIS_HORIZONTAL(水平方向滚动)
+     * @return :true表示本次滚动支持嵌套滚动,false不支持
+     * */
     @Override
     public boolean startNestedScroll(int axes) {
         Log.d(TAG, "startNestedScroll: axes" + axes);
@@ -116,6 +134,15 @@ public class NestedChildView extends View implements NestedScrollingChild {
         return b;
     }
 
+    /**
+     * 分发嵌套滚动的操作给当前View嵌套滚动的父级.这是一个代理委托方法
+     * @param dxConsumed 表示view消费了x方向的距离长度
+     * @param dyConsumed 表示view消费了y方向的距离长度
+     * @param dxUnconsumed 表示滚动产生的x滚动距离还剩下多少没有消费
+     * @param dyUnconsumed 表示滚动产生的y滚动距离还剩下多少没有消费
+     * @param offsetInWindow  表示剩下的距离dxUnconsumed和dyUnconsumed使得view在父布局中的位置偏移了多少
+     * @return 如果父级消费了嵌套滑动距离，返回true
+     * */
     @Override
     public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, @Nullable int[] offsetInWindow) {
         Log.d(TAG, "dispatchNestedScroll: dxConsumed : " + dxConsumed + " dyConsumed: " + dyConsumed + " dxUnconsumed: " + dxUnconsumed + " dyUnconsumed: " + dyUnconsumed + " offsetInWindow : " + Arrays.toString(offsetInWindow));
@@ -123,6 +150,14 @@ public class NestedChildView extends View implements NestedScrollingChild {
         return childHelper.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow);
     }
 
+    /**
+     * 执行嵌套滑动操作之前，通知父级
+     * @param dx 表示view本次x方向的滚动的总距离长度
+     * @param dy 表示view本次y方向的滚动的总距离长度
+     * @param consumed 表示父布局消费的距离,consumed[0]表示x方向,consumed[1]表示y方向
+     * @param offsetInWindow 表示剩下的距离dxUnconsumed和dyUnconsumed使得view在父布局中的位置偏移了多少
+     * @return 如果父级消费嵌套滑动返回 true
+     * */
     @Override
     public boolean dispatchNestedPreScroll(int dx, int dy, @Nullable int[] consumed, @Nullable int[] offsetInWindow) {
 //        boolean b = super.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow);
@@ -131,6 +166,9 @@ public class NestedChildView extends View implements NestedScrollingChild {
         return b;
     }
 
+    /**
+     * 将嵌套的fling操作发送到当前嵌套的滚动父级。
+     * */
     @Override
     public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
         Log.d(TAG, "dispatchNestedFling: velocityX : " + velocityX + " velocityY: " + velocityY + " consumed: " + consumed);
@@ -138,6 +176,11 @@ public class NestedChildView extends View implements NestedScrollingChild {
         return childHelper.dispatchNestedFling(velocityX, velocityY, consumed);
     }
 
+    /**
+     * 将嵌套的预先执行操作分派给当前嵌套的滚动父级
+     * @param velocityX x滚动速率
+     * @param velocityY y滚动速度
+     * */
     @Override
     public boolean dispatchNestedPreFling(float velocityX, float velocityY) {
         Log.d(TAG, "dispatchNestedPreFling: velocityX : " + velocityX + " velocityY :" + velocityY);
--- app/src/main/java/com/example/viewpractice/widgets/NestedParentFrameLayout.java
+++ app/src/main/java/com/example/viewpractice/widgets/NestedParentFrameLayout.java
@@ -18,7 +18,17 @@ import androidx.core.view.ViewCompat;
 
 /**
  * 作者：wxz11 on 2019/3/8 16:53
- * Email : wangc@huitouche.com
+ * 作为一个可以嵌入 NestedScrollingChild 的父 View，需要实现 NestedScrollingParent，这个接口方法和 NestedScrollingChild 大致有一一对应的关系。
+ * 同样，也有一个 NestedScrollingParentHelper 辅助类来默默的帮助你实现和 Child 交互的逻辑。
+ * 滑动动作是 Child 主动发起，Parent 就收滑动回调并作出响应。
+ *
+ * 从Child 分析可知，滑动开始的调用 startNestedScroll()，Parent 收到 onStartNestedScroll() 回调，决定是否需要配合 Child 一起进行处理滑动，如果需要配合，还会回调 onNestedScrollAccepted()。
+ *
+ * 每次滑动前，Child 先询问 Parent 是否需要滑动，即 dispatchNestedPreScroll()，这就回调到 Parent 的 onNestedPreScroll()，Parent 可以在这个回调中“劫持”掉 Child 的滑动，也就是先于 Child 滑动。
+ *
+ * Child 滑动以后，会调用 onNestedScroll()，回调到 Parent 的 onNestedScroll()，这里就是 Child 滑动后，剩下的给 Parent 处理，也就是 后于 Child 滑动。
+ *
+ * 最后，滑动结束，调用 onStopNestedScroll() 表示本次处理结束。
  */
 public class NestedParentFrameLayout extends FrameLayout implements NestedScrollingParent {
     private static final String TAG = "NestedParentFrameLayout";
@@ -49,6 +59,13 @@ public class NestedParentFrameLayout extends FrameLayout implements NestedScroll
         mParentHelper = new NestedScrollingParentHelper(this);
     }
 
+    /**
+     * 有嵌套滑动到来了，问下该父View是否接受嵌套滑动
+     * @param child 嵌套滑动对应的父类的子类(因为嵌套滑动对于的父View不一定是一级就能找到的，可能挑了两级父View的父View，child的辈分>=target)
+     * @param target 具体嵌套滑动的那个子类
+     * @param nestedScrollAxes 支持嵌套滚动轴。水平方向，垂直方向，或者不指定
+     * @return 是否接受该嵌套滑动
+     */
     @Override
     public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int nestedScrollAxes) {
         Log.d(TAG, "onStartNestedScroll : child " + child + " target : " + target + " nestedScrollAxes : " + nestedScrollAxes);
@@ -59,7 +76,10 @@ public class NestedParentFrameLayout extends FrameLayout implements NestedScroll
 //        return super.onStartNestedScroll(child, target, nestedScrollAxes);
     }
 
-
+    /**
+     * 该View接受了嵌套滑动的请求该函数调用。onStartNestedScroll返回true该函数会被调用。
+     * 参数和onStartNestedScroll一样
+     */
     @Override
     public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes) {
         Log.d(TAG, "onNestedScrollAccepted : child " + child + " target : " + target + " axes : " + axes);
@@ -67,6 +87,9 @@ public class NestedParentFrameLayout extends FrameLayout implements NestedScroll
         mParentHelper.onNestedScrollAccepted(child, target, axes);
     }
 
+    /**
+     * 停止嵌套滑动
+     * */
     @Override
     public void onStopNestedScroll(@NonNull View child) {
         Log.d(TAG, "onStopNestedScroll : child " + child);
@@ -74,12 +97,29 @@ public class NestedParentFrameLayout extends FrameLayout implements NestedScroll
         mParentHelper.onStopNestedScroll(child);
     }
 
+    /**
+     * 嵌套滑动的子View在滑动之后报告过来的滑动情况
+     *
+     * @param target 具体嵌套滑动的那个子类
+     * @param dxConsumed 水平方向嵌套滑动的子View滑动的距离(消耗的距离)
+     * @param dyConsumed 垂直方向嵌套滑动的子View滑动的距离(消耗的距离)
+     * @param dxUnconsumed 水平方向嵌套滑动的子View未滑动的距离(未消耗的距离)
+     * @param dyUnconsumed 垂直方向嵌套滑动的子View未滑动的距离(未消耗的距离)
+     */
     @Override
     public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         Log.d(TAG, "onNestedScroll : target " + target + " dxConsumed:" + dxConsumed + " dyConsumed: " + dyConsumed + " dxUnconsumed : " + dxUnconsumed + " dyUnconsumed: " + dyUnconsumed);
         super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
     }
 
+    /**
+     * 嵌套滑动的子View在滑动操作之前告诉父级，滑动情况
+     * @param target 嵌套滑动的具体子类
+     * @param dx 嵌套滑动子View在x方向想要滑动的距离
+     * @param dy 嵌套滑动子View在y方向想要滑动的距离
+     * @param consumed 这个参数要我们在实现这个函数的时候指定，回头告诉子View当前父View消耗的距离，
+     *  consumed[0] 水平消耗的距离，consumed[1] 垂直消耗的距离 好让子view做出相应的调整
+     * */
     @Override
     public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed) {
         Log.d(TAG, "onNestedPreScroll: target : " + target + " dx : " + dx + " dy : " + dy + " consumed : " + Arrays.toString(consumed));
@@ -111,18 +151,38 @@ public class NestedParentFrameLayout extends FrameLayout implements NestedScroll
 
     }
 
+    /**
+     * 嵌套滑动的子View在fling之后告诉父级的fling情况
+     * @param target 具体嵌套滑动的子类
+     * @param velocityX
+     * @param velocityY
+     * @param consumed 子View是否消费了fling事件
+     * @return 是否消费了fling事件
+     * */
     @Override
     public boolean onNestedFling(@NonNull View target, float velocityX, float velocityY, boolean consumed) {
         Log.d(TAG, "onNestedFling: target : " + target + " velocityX : " + velocityX + " velocityY : " + velocityY + " consumed : " + consumed);
         return super.onNestedFling(target, velocityX, velocityY, consumed);
     }
 
+    /**
+     * 嵌套滑动的子View在fling之前告诉fling的情况
+     * @param target 具体嵌套滑动的子类
+     * @param velocityX 水平方向的速度
+     * @param velocityY
+     * */
     @Override
     public boolean onNestedPreFling(@NonNull View target, float velocityX, float velocityY) {
         Log.d(TAG, "onNestedPreFling: target : " + target + " velocityX : " + velocityX + " velocityY : " + velocityY);
         return super.onNestedPreFling(target, velocityX, velocityY);
     }
 
+    /**
+     *获取当前ViewGroup嵌套滑动的轴，水平滑动，还是垂直滑动
+     * @see ViewCompat#SCROLL_AXIS_HORIZONTAL 垂直
+     * @see ViewCompat#SCROLL_AXIS_VERTICAL 水平
+     * @see ViewCompat#SCROLL_AXIS_NONE 都支持
+     * */
     @Override
     public int getNestedScrollAxes() {
 //        return super.getNestedScrollAxes();
